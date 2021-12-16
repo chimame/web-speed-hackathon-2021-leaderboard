6c2103750446d3a9339340719b4638344638eef1
chimame <rito.tamata@gmail.com>
2021-12-16 19:26:10 +0900

    Add supabase-js

---
+++ scripts/leaderboard/app/type.d.ts
@@ -0,0 +1,6 @@
+export {}
+
+declare global {
+  const SUPABASE_URL: string | undefined
+  const SUPABASE_ANON_KEY: string | undefined
+}
---
+++ scripts/leaderboard/app/util/supabaseClient.ts
@@ -0,0 +1,5 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = SUPABASE_URL || ''
+const supabaseAnonKey = SUPABASE_ANON_KEY || ''
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
