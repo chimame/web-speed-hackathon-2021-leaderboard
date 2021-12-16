3b65ecb7d031fa6307f00481b8bc9bd7757c135b
chimame <rito.tamata@gmail.com>
2021-12-17 01:49:47 +0900

    Add SignIn page

--- scripts/leaderboard/tailwind.config.js
+++ scripts/leaderboard/tailwind.config.js
@@ -1,10 +1,12 @@
 module.exports = {
   mode: "jit",
-  purge: ["./app**/*.{ts,tsx}"],
+  purge: ["./app/**/*.{ts,tsx}"],
   darkMode: "media",
   theme: {
     extend: {}
   },
   variants: {},
-  plugins: []
+  plugins: [
+    require('@tailwindcss/forms'),
+  ]
 }
